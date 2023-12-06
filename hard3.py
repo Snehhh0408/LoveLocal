c4870af0652cbf59794c3d4583f2c3ccc1c36e77
Snehhh0408 <99189586+Snehhh0408@users.noreply.github.com>
2023-12-07 00:09:48 +0530

    Update hard3.py

--- hard3.py
+++ hard3.py
@@ -1,6 +1,7 @@
-//uses an iterative approach to count the occurrences of the first digit (1 to 9) in all numbers from 0 up to the given number n.
-//It uses a while loop that iterates through the digit places (1s, 10s, 100s, etc.) by multiplying i by 10 in each iteration. For each digit place, it calculates how many times the first digit appears within that place range.
-// it calculates the occurrences of the first digit and accumulates these counts
+#PROJECT DESCRIPTION
+#uses an iterative approach to count the occurrences of the first digit (1 to 9) in all numbers from 0 up to the given number n.
+#It uses a while loop that iterates through the digit places (1s, 10s, 100s, etc.) by multiplying i by 10 in each iteration. For each digit place, it calculates how many times the first digit appears within that place range.
+#it calculates the occurrences of the first digit and accumulates these counts
 
 
 def first_digit_count(n):
