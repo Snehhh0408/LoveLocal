2bd831758dbb4c2979d63585aeb0f5ffa86eba64
Snehhh0408 <99189586+Snehhh0408@users.noreply.github.com>
2023-12-07 00:12:15 +0530

    Update easy.py

--- easy.py
+++ easy.py
@@ -1,3 +1,9 @@
+#PROJECT DESCRIPTION
+#takes an input sentence, strips any trailing whitespace, and then splits the sentence into words using the space character as a delimiter.
+#It returns the length of the last word in the input sentence.
+#It retrieves the last word from the list of words obtained and computes its length using the len() function. 
+#If the input sentence is empty or has no words after stripping, it returns 0.
+
 def lengthh(s):
     words = s.rstrip().split()
     if not words:
